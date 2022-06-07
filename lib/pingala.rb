98dcc1aff41f4e4f2e1b210dde3fa6ed72b067e9
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-06-07 21:10:36 +0200

    Write recursive pingala method with simple answer
    1 line, quite fun!

--- lib/pingala.rb
+++ lib/pingala.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Fibnonacci number witt iteration
+# Fibnonacci number with iteration
 def pingala(n)
   fibs = []
   fibs[0] = 0
@@ -9,4 +9,9 @@ def pingala(n)
     fibs[number] = fibs[number - 1] + fibs[number - 2]
   end
   p fibs
+end
+
+# Fibonacci number with recursion, simple answer
+def pingala_recursion(n)
+  n <= 1 ? n : pingala_recursion(n - 1) + pingala_recursion(n - 2)
 end
