dc8dc45fa908dae6d5ba0b9b98519dee10fac031
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-06-07 21:38:19 +0200

    Add method for recursive fibs with array answer
    3 lines, nice!

--- lib/pingala.rb
+++ lib/pingala.rb
@@ -15,3 +15,11 @@ end
 def pingala_recursion(n)
   n <= 1 ? n : pingala_recursion(n - 1) + pingala_recursion(n - 2)
 end
+
+# Fibonacci number with recursion, array answer
+def pingala_rec_array(n)
+  (return n.zero? ? [0] : [0, 1]) if n <= 1
+
+  fibs = pingala_rec_array(n - 1)
+  fibs << fibs[-2] + fibs[-1]
+end
